a188f28661b98194d809b1b974c25537f2cfa6e5
Anna Hurfinkel <ann.gurfinkel@gmail.com>
2025-10-07 16:15:11 +0300

    Опис змін

--- tests/testexample_organization_management.spec.ts
+++ tests/testexample_organization_management.spec.ts
@@ -1,26 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('terms_of_use', async ({ page }) => {
+test('Organization_management', async ({ page }) => {
   await page.goto('https://platform.labelyourdata.com/sign-in');
-
+  await page.getByTestId('sign-in-username').click();
   await page.getByTestId('sign-in-username').fill('client_test1');
+  await page.getByTestId('sign-in-password').click();
   await page.getByTestId('sign-in-password').fill('Fjik67%ips');
   await page.getByTestId('sign-in-btn').click();
-
-  const userInitials = page.getByText('CL').first();
-  await expect(userInitials).toBeVisible({ timeout: 10000 });
-  await userInitials.click();
-
-  const termsLink = page.getByRole('link', { name: 'Terms of use' });
-  await expect(termsLink).toBeVisible({ timeout: 10000 });
-
-  await Promise.all([
-    page.waitForNavigation({ waitUntil: 'networkidle' }),
-    termsLink.click(),
-  ]);
-
-  await expect(page).toHaveURL(/terms-of-use/i, { timeout: 10000 });
-
-  // Перевірка конкретного заголовку з текстом "Terms of use"
-  await expect(page.getByRole('heading', { name: /terms of use/i })).toBeVisible({ timeout: 10000 });
+  await page.getByText('CL').first().click();
+  await page.getByRole('link', { name: 'Organization management' }).click();
 });
